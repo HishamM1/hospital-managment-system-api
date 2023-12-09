957f173ee9e739587793154626b3a490a8ca08d8
HishamM1 <hishammedhat0@gmail.com>
2023-12-09 18:55:48 +0200

    Remove godotenv dependency from ConnectDB function

--- config/config.go
+++ config/config.go
@@ -3,18 +3,12 @@ package config
 import (
 	"os"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 // ConnectDB connects go to mysql database
 func ConnectDB() *gorm.DB {
-	errorENV := godotenv.Load()
-	if errorENV != nil {
-		panic("Failed to load env file")
-	}
-
 	db, errorDB := gorm.Open(mysql.Open(os.Getenv("DSN")), &gorm.Config{})
 	if errorDB != nil {
 		panic("Failed to connect mysql database")
