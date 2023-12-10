373c3790cceb9c3759eede051fb1f8cb5fcb5afb
HishamM1 <hishammedhat0@gmail.com>
2023-12-10 12:31:38 +0200

    Add columns to doctors and numbers tables

--- db/migrations/20231208142303_doctors.sql
+++ db/migrations/20231208142303_doctors.sql
@@ -1,7 +1,10 @@
 -- migrate:up
 CREATE TABLE doctors (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL,
+    number VARCHAR(20) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    specialization VARCHAR(255) NOT NULL
 );
 
 -- migrate:down
--- db/migrations/20231208142501_numbers.sql
+++ db/migrations/20231208142501_numbers.sql
@@ -1,9 +1,9 @@
 -- migrate:up
 CREATE TABLE numbers (
     id INT PRIMARY KEY AUTO_INCREMENT,
-	patient_id INT,
-	number VARCHAR(20) NOT NULL,
-	description VARCHAR(50) NOT NULL,
+	patient_id INT NOT NULL,
+	patient_number VARCHAR(20) NOT NULL,
+	family_number VARCHAR(20) NOT NULL,
 	FOREIGN KEY (patient_id) REFERENCES patients(id)
 );
 
